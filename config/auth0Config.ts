c7cdbfbf5134c9c9b0bd7c9fbebecee13d416ffc
kunal rathore <kunal.rathore@ciphercru.com>
2024-09-26 18:44:17 +0530

    fix auth issue

--- config/auth0Config.ts
+++ config/auth0Config.ts
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-    audience:'http://localhost:8000/',
+    audience:'https://prisma-ks.netlify.app/',
     issuerBaseURL:'https://dev-01jqlp4png236jat.us.auth0.com',
     tokenSigningAlg:'RS256'
 })
